210df90086f0ae746401639529a0c55058716ea1
Dan Ristea <danristea@protonmail.com>
2024-05-30 16:15:55 +0100

    Update experiment script

--- submit.sh
+++ submit.sh
@@ -4,13 +4,14 @@
 #SBATCH --gpus-per-task 1
 #SBATCH --tasks-per-node 1
 #SBATCH --nodes 1
-#SBATCH --time 1:0:0
+#SBATCH --time 3:0:0
 
 module purge
 module load baskerville
 module load bask-apps/live
 module load Python/3.11.3-GCCcore-12.3.0
+module load CUDA/12.1.1
 
 cd ~/pax
 source .venv/bin/activate
-python -m pax.experiment +experiment/ipd=shaper_v_ppo_mem
+python3 -m pax.experiment +experiment/ipd=ppo_v_ppo num_iters=100
